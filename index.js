4ee0aa4ef0c25a738ca3788006d2c4e50c324778
Fabio <fabio04musitelli@gmail.com>
2024-10-25 15:44:48 +0200

    Add cors middleware for handling cross-origin requests

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require('express');
-
+const cors = require('cors')
 const app = express();
 const port = 3000;
+
+app.use(express.json());
+app.use(cors({
+    origin: true, // Allows requests from any origin
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true, // Allows cookies if needed
+}));
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
