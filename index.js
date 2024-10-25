c7967b8d4c2fbaaf5b7189d2f93f19c48cfca4fc
Fabio <fabio04musitelli@gmail.com>
2024-10-25 15:40:13 +0200

    Add /helloboss endpoint

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
+app.get("/helloboss", (req, res) => {
+    res.send("Hello Boss");
+});
+
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
