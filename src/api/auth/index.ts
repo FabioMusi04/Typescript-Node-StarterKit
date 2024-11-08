d38fe76de72f61aeadbe230a48318e4e25cc9dd3
Fabio <fabio04musitelli@gmail.com>
2024-11-08 09:40:27 +0100

    feat: add JWT authentication middleware and new endpoint to retrieve user info

--- src/api/auth/index.ts
+++ src/api/auth/index.ts
@@ -1,10 +1,15 @@
 import { Router } from 'express';
 import { login, register } from './controller.ts';
+import { authenticate } from '../../services/auth/auth.ts';
 
 const router = Router();
 
 router.post('/register', register);
 
 router.post('/login', login);
+
+router.get('/me', authenticate(), (req, res) => {
+    res.json(req.user);
+});
 
 export default router;
--- src/services/auth/auth.ts
+++ src/services/auth/auth.ts
@@ -6,6 +6,8 @@ import config from '../../config.ts';
 import { JwtPayload } from './jwt.ts';
 import User, { IUser } from '../../api/users/model.ts';
 
+import { Strategy as JWTstrategy, ExtractJwt } from 'passport-jwt';
+
 const secretKey = config.jwtSecret;
 const masterKey = config.masterKey;
 
@@ -40,28 +42,48 @@ passport.use(
 );
 
 
-export const token = (requiredRole?: UsersRoleEnum, useMasterKey: boolean = false) => {
+passport.use(
+    new JWTstrategy(
+        {
+            secretOrKey: secretKey,
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken()
+        },
+        async (token, done) => {
+            console.log(token);
+            try {
+                return done(null, token.user);
+            } catch (error) {
+                done(error);
+            }
+        }
+    )
+);
+
+export const authenticate = (hasMasterKey?: boolean, roles?: UsersRoleEnum[], ) => {
     return (req: Request, res: Response, next: NextFunction) => {
-        interface AuthenticatedRequest extends Request {
-            user?: JwtPayload;
-        }
+        passport.authenticate('jwt', { session: false }, (err: any, user: IUser | undefined) => {
+            if (err) {
+                return next(err);
+            };
 
-        const token = req.headers.authorization?.split(' ')[1];
+            const authHeader = req.headers['authorization'];
+            
+            const tokenMasterKey = typeof authHeader === 'string' ? authHeader.split(' ')[1] : undefined;
 
-        if (useMasterKey && token === masterKey) {
-            return next();
-        }
-
-        passport.authenticate('jwt', { session: false }, (err: any, user: JwtPayload, info: any) => {
-            if (err || !user) {
-                return res.status(401).json({ message: 'Unauthorized: Invalid or missing token' });
+            if (hasMasterKey && tokenMasterKey === masterKey) {
+                req.user = { role: UsersRoleEnum.ADMIN, email: "admin", username: "admin" } as IUser;
+                return next();
             }
 
-            if (requiredRole && user.role !== requiredRole) {
-                return res.status(403).json({ message: 'Forbidden: Insufficient role' });
+            if (user && roles?.length && !roles.includes(user.role)) {
+                return res.status(403).send('Forbidden');
             }
 
-            (req as AuthenticatedRequest).user = user;
+            if (!user) {
+                return res.status(401).send('Unauthorized');
+            }
+
+            req.user = user;
             next();
         })(req, res, next);
     };
