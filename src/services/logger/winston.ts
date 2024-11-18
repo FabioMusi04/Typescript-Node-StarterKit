ccb88e5e910006992c3a34793dc1b25e88c6ac04
Fabio <fabio04musitelli@gmail.com>
2024-11-18 10:28:28 +0100

    feat: update dependencies for logform and winston; enhance logger colorization and formatting

--- src/services/logger/winston.ts
+++ src/services/logger/winston.ts
@@ -1,44 +1,49 @@
+import { TransformableInfo } from 'logform';
 import winston from 'winston';
 const { combine, timestamp, colorize, printf } = winston.format;
 
 winston.addColors({
-  get: 'green',
+  success: 'green',
+  redirection: 'yellow',
+  clientError: 'red',
+  serverError: 'magenta',
+  get: 'cyan',
   post: 'blue',
   put: 'yellow',
   delete: 'red',
   patch: 'magenta',
-  success: 'green',  // For 2xx status codes
-  redirection: 'yellow', // For 3xx status codes
-  clientError: 'red', // For 4xx status codes
-  serverError: 'magenta', // For 5xx status codes
 });
 
+
 const getStatusCodeColor = (statusCode: number) => {
-  if (statusCode >= 200 && statusCode < 300) {
-    return 'success'; // Green for 2xx
-  } else if (statusCode >= 300 && statusCode < 400) {
-    return 'redirection'; // Yellow for 3xx
-  } else if (statusCode >= 400 && statusCode < 500) {
-    return 'clientError'; // Red for 4xx
-  } else if (statusCode >= 500 && statusCode < 600) {
-    return 'serverError'; // Magenta for 5xx
-  }
-  return 'gray'; // Default color for other status codes
+  if (statusCode >= 200 && statusCode < 300) return 'success';
+  if (statusCode >= 300 && statusCode < 400) return 'redirection';
+  if (statusCode >= 400 && statusCode < 500) return 'clientError';
+  if (statusCode >= 500 && statusCode < 600) return 'serverError';
+  return undefined; // Use default log color
 };
-const httpCustomFormat = printf(({ level, message, timestamp, ...metadata }) => {
-  const [method, url, statusCode] = message.split(' ');
 
-  const coloredMethod = winston.format.colorize().colorize(method.toLowerCase(), method);
-  const coloredStatusCode = winston.format.colorize().colorize(getStatusCodeColor(Number(statusCode)), statusCode);
 
-  return `${timestamp} [${level}] ${coloredMethod} ${url} ${coloredStatusCode} ${message.split(' ').slice(3).join(' ')} ${JSON.stringify(metadata)}`;
+const httpCustomFormat = printf((info: winston.Logform.TransformableInfo) => {
+  const { level, message, timestamp, ...metadata } = info;
+  const [method, url, statusCode] = (message as string).split(' ');
+
+  const coloredMethod = winston.format.colorize().colorize(method.toLowerCase() || 'default', method || 'METHOD');
+  const coloredStatusCode = winston.format.colorize().colorize(
+    getStatusCodeColor(Number(statusCode)) || 'default',
+    statusCode.toString()
+  );
+
+
+
+  return `${timestamp} [${level}] ${coloredMethod} ${url} ${coloredStatusCode} ${(message as string).split(' ').slice(3).join(' ')} ${JSON.stringify(metadata)}`;
 });
 
 const httpLogger = winston.createLogger({
   level: 'http',
   format: combine(
     timestamp(),
-    httpCustomFormat 
+    httpCustomFormat
   ),
   transports: [
     new winston.transports.Console({
@@ -67,12 +72,12 @@ const generalLogger = winston.createLogger({
 });
 
 export const requestLogger = (req: { method: any; url: any; }, res: { on?: any; statusCode?: any; }, next: () => void) => {
-  const start = Date.now(); 
+  const start = Date.now();
 
   res.on('finish', () => {
     const { method, url } = req;
     const { statusCode } = res;
-    const responseTime = Date.now() - start;  
+    const responseTime = Date.now() - start;
 
     httpLogger.http(`${method} ${url} ${statusCode} ${responseTime}ms`);
   });
