5c8bec164e61231c557c352029e435ea397be6f6
FabioMusi04 <76118020+FabioMusi04@users.noreply.github.com>
2025-02-14 18:29:56 +0100

    fix: upload logic, added populationSystem, improved QueryFilters

--- src/utils/enum.ts
+++ src/utils/enum.ts
@@ -1,4 +1,18 @@
 export enum UsersRoleEnum {
     "USER" = "user",
-    "ADMIN" = "admin"   
+    "ADMIN" = "admin"
 }
+export const UsersRoleEnumValues = Object.values(UsersRoleEnum);
+
+export enum ReservationStatusEnum {
+    "CONFIRMED" = 'confirmed',
+    "PENDING" = 'pending',
+    "CANCELLED" = 'cancelled'
+}
+export const ReservationStatusValues = Object.values(ReservationStatusEnum);
+
+export enum LinkedEntityTypeEnum {
+    "VOUCHER_IMAGE" = 'voucherImage',
+    "VOUCHER_COVER_IMAGE" = 'voucherCoverImage',
+}
+export const LinkedEntityTypeValues = Object.values(LinkedEntityTypeEnum);
