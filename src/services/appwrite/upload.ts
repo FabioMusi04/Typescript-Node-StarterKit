ae023beda7767a9009353d88f048bea4ab35e90a
Marco Bertelli <m.bertelli@anoki.it>
2025-02-18 10:36:55 +0100

    fix: update uploadImage function to accept item object and adjust linked entity type

--- src/services/appwrite/upload.ts
+++ src/services/appwrite/upload.ts
@@ -1,14 +1,15 @@
-import UploadedFile from "../../api/uploadedFiles/model.ts";
+import UploadedFile, { IUploadedFile } from "../../api/uploadedFiles/model.ts";
 import Config from "../../config.ts";
 
 import { ID } from "node-appwrite";
 import { storage } from "./index.ts";
 import { InputFile } from "node-appwrite/file";
 import { generalLogger } from "../logger/winston.ts";
-import { IVoucher } from "../../api/vouchers/model.ts";
 import { LinkedEntityTypeEnum } from "../../utils/enum.ts";
+import { Schema } from "mongoose";
 
-export async function uploadImage(fileBuffer: Buffer, fileName: string, voucher: IVoucher, linkedEntityType: LinkedEntityTypeEnum = LinkedEntityTypeEnum.VOUCHER_IMAGE): Promise<UploadedFile> {
+
+export async function uploadImage(fileBuffer: Buffer, fileName: string, item: { userId: Schema.Types.ObjectId, _id: Schema.Types.ObjectId }, linkedEntityType: LinkedEntityTypeEnum = LinkedEntityTypeEnum.IMAGE): Promise<IUploadedFile> {
     try {
         const response = await storage.createFile(
             Config.appwrite.bucketUploadsId,
@@ -19,13 +20,13 @@ export async function uploadImage(fileBuffer: Buffer, fileName: string, voucher:
             ));
 
         const uploadedFile = await UploadedFile.create({
-            uploaderId: voucher.userId,
+            uploaderId: item.userId,
             documentType: 'image',
             originalName: fileName,
             full: response.$id,
             linkedEntity: {
                 linkedEntityType: linkedEntityType,
-                linkedEntityId: voucher._id,
+                linkedEntityId: item._id,
             },
             metadata: {},
             thumbnail: {},
